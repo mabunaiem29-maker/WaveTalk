7574d01d5f536d85e8852b0ce323a2217ef39c85
mabunaiem29-maker <mabunaiem29@gmail.com>
2026-07-02 23:46:47 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function hello(){
-  document.getElementById("msg").innerHTML =
-  "WaveTalk Successfully Running 🚀";
+function join(){
+
+let name =
+document.getElementById("name").value;
+
+document.getElementById("msg").innerHTML =
+"Welcome " + name + " 🚀";
+
 }
